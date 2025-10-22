77b3a067a2b8e3ffd51d663b71f81a0890c02a64
Parsa Asgari <parsa.asgari3264@gmail.com>
2025-10-22 21:37:30 +0330

    added python files + test

---
+++ test_calculator.py
@@ -0,0 +1,9 @@
+from calculator import add, multiply
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_multiply():
+    assert multiply(3, 2) == 6
+    assert multiply(2, 21) == 42
